1e958571514fbe1fae5bc2fdc4947b8123cbb414
ducshntrong <huynhtrongduc01225445811@gmail.com>
2025-06-17 15:21:37 +0700

    main

--- app/src/main/java/com/example/tipcalculate/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculate/MainActivity.kt
@@ -8,7 +8,7 @@ import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    //edit1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
